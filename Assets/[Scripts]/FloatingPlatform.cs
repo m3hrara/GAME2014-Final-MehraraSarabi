713ef0ce2317f2819568ee0f7a397cb8330483ec
m3hrara <mehrarasb@gmail.com>
2021-12-14 16:27:45 -0500

    Floating platform shrink and resize

--- Assets/[Scripts]/FloatingPlatform.cs
+++ Assets/[Scripts]/FloatingPlatform.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FloatingPlatform : MonoBehaviour
 {
+    private float localX;
     public float maxY;
     public float minY;
     private bool playerOnPlatform = false;
@@ -13,10 +14,11 @@ public class FloatingPlatform : MonoBehaviour
     {
         maxY = transform.position.y + 0.5f;
         minY = transform.position.y - 0.5f;
+        localX = transform.localScale.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // platform slightly moves up and down when player is NOT on it
         if(!playerOnPlatform)
@@ -38,24 +40,29 @@ public class FloatingPlatform : MonoBehaviour
                     goUp = true;
                 }
             }
+            // go back to original size if it's been shrunk
+            if(transform.localScale.x<localX)
+            {
+                this.transform.localScale = new Vector3(this.transform.localScale.x + 0.01f, this.transform.localScale.y, this.transform.localScale.z);
+            }
         }
         // start shrinking when player is on platform
         else if(playerOnPlatform)
         {
-
+            this.transform.localScale = new Vector3(this.transform.localScale.x - 0.01f, this.transform.localScale.y, this.transform.localScale.z);
         }
         
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Floating")
+        if(collision.gameObject.name == "Player")
         {
             playerOnPlatform = true;
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Floating")
+        if (collision.gameObject.name == "Player")
         {
             playerOnPlatform = false;
         }
